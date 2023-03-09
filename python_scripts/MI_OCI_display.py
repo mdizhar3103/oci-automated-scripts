f33a82fbbbc1f0e7524fdf95a32f372d400f9fe9
Mohd Izhar <89973968+mdizhar3103@users.noreply.github.com>
2023-03-09 18:48:03 +0530

    adding ipsec tunnel display

--- python_scripts/MI_OCI_display.py
+++ python_scripts/MI_OCI_display.py
@@ -33,6 +33,9 @@ for comp_name, comp_id in compartments.items():
 
     # Get DB Systems
     display_db_systems(comp_id)
+    
+    # Get Ipsec Tunnels
+    display_ipsec_tunnels(comp_id)
 
     # Get Load Balancers
     display_load_balancers(comp_id)
